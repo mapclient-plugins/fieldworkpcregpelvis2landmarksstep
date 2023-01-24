8c72636bdbb2d6feafcf5af0bfa1c295df45b94f
TimmyNic <tim.nicolas@outlook.com>
2023-01-24 13:05:01 +1300

    Upgrade to PySide6.

--- mapclientplugins/fieldworkpcregpelvis2landmarksstep/step.py
+++ mapclientplugins/fieldworkpcregpelvis2landmarksstep/step.py
@@ -5,7 +5,7 @@ import os
 import json
 import copy
 
-from PySide2 import QtGui
+from PySide6 import QtGui
 
 from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
 from mapclientplugins.fieldworkpcregpelvis2landmarksstep.configuredialog import ConfigureDialog
